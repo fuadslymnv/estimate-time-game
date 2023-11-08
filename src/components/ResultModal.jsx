d34289b5c4f1a407794027c8a46d3b9d1e5d3383
fuadslymnv <fuadsuleymanov03@gmail.com>
2023-11-08 22:55:00 +0400

    ResultModal component is added

---
+++ src/components/ResultModal.jsx
@@ -0,0 +1,14 @@
+export default function ResultModal({ result, targetTime }) {
+  return (
+    <dialog className="result-modal" open> 
+      <h2>You {result}</h2>
+      <p>Target time was <strong>{targetTime} seconds.</strong></p>
+      <p>
+        You stopped the timer with <strong>X seconds left.</strong>
+      </p>
+      <form action="dialog">
+        <button>Close</button>
+      </form>
+    </dialog>
+  );
+}
